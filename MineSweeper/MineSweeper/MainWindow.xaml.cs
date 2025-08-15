ff1668622263fa26ddb30853a9d7bce45efff44e
suraj-p-nair <asn.surajpnairofficial@gmail.com>
2025-08-15 19:03:22 +0530

    Adding header and MineField
    Created a window with 2 rows
    1st row for header
    2nd row for the minefield
    2nd row when app started gets rendered with placeholder uniform grid with the cell number as content

--- MineSweeper/MineSweeper/MainWindow.xaml.cs
+++ MineSweeper/MineSweeper/MainWindow.xaml.cs
@@ -1,24 +1,26 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MineSweeper
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            CreateBoard();
+        }
+
+        private void CreateBoard()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Button btn = new()
+                {
+                    Content = $"{i}"
+                };
+                MineField.Children.Add(btn);
+            }
         }
     }
 }
