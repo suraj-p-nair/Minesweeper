9a9655433ef3db322a03947eed4168a1aadd33fa
suraj-p-nair <asn.surajpnairofficial@gmail.com>
2025-08-15 20:22:00 +0530

    test

--- MineSweeper/.github/hooks/pre-commit.bat
+++ MineSweeper/.github/hooks/pre-commit.bat
@@ -1,24 +1,3 @@
 @echo off
-setlocal enabledelayedexpansion
-
-README_PATH="Minesweeper/README.md"
-
-:: Get the commit message
-set /p COMMIT_MSG=<.git\COMMIT_EDITMSG
-
-:: Read the rest as description
-(for /f "skip=1 delims=" %%i in (.git\COMMIT_EDITMSG) do (
-    echo %%i
-)) > temp_desc.txt
-
-:: Append to README.md
-echo. >> "$README_PATH"
-echo ## **%COMMIT_MSG%** >> README.md
-echo. >> "$README_PATH"
-type temp_desc.txt >> README.md
-echo. >> "$README_PATH"
-
-del temp_desc.txt
-
-:: Stage README.md
-git add "$README_PATH"
+echo Pre-commit hook fired! > "%~dp0hook_test.txt"
+exit 0
