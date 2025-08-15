d48c63ed5c90e73bf1c57659b4beda43daec5456
suraj-p-nair <asn.surajpnairofficial@gmail.com>
2025-08-15 20:05:56 +0530

    GitHook Test
    Trying GitHooks to capture commit message and edit the readme
    
    added pre-commit hook which captures the commit message and updates the readme
    
    added install-hook file to copy the tracked pre-commit hook into untracked .git folder

--- MineSweeper/.github/hooks/pre-commit.bat
+++ MineSweeper/.github/hooks/pre-commit.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
+README_PATH="Minesweeper/README.md"
+
 :: Get the commit message
 set /p COMMIT_MSG=<.git\COMMIT_EDITMSG
 
@@ -10,13 +12,13 @@ set /p COMMIT_MSG=<.git\COMMIT_EDITMSG
 )) > temp_desc.txt
 
 :: Append to README.md
-echo. >> README.md
+echo. >> "$README_PATH"
 echo ## **%COMMIT_MSG%** >> README.md
-echo. >> README.md
+echo. >> "$README_PATH"
 type temp_desc.txt >> README.md
-echo. >> README.md
+echo. >> "$README_PATH"
 
 del temp_desc.txt
 
 :: Stage README.md
-git add README.md
+git add "$README_PATH"
